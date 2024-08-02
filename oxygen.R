3b1ea3bb6c1750dc818affe4716b24e5ff7ff328
JonasStage <Jonasstage@hotmail.com>
2024-08-02 13:54:47 +0200

    oxygen supplementary plots

---
+++ oxygen.R
@@ -0,0 +1,25 @@
+library(tidyverse);library(lubridate)
+
+read_csv("/Users/jonas/Library/CloudStorage/OneDrive-SyddanskUniversitet/Gribskov/Environmental variables/ilt.csv") -> oxy
+source("/Users/jonas/Library/CloudStorage/OneDrive-SyddanskUniversitet/R help script/ggplot_themes.R") ### getting ggplot theme
+
+summary(oxy)
+
+oxy %>% 
+  filter(between(datetime, ymd_hms("2023-02-14 06:00:00"),ymd_hms("2024-02-07 08:00:00"))) %>% 
+  ggplot(aes(datetime)) +
+  geom_line(aes(y = do, col = "do")) + 
+  geom_line(aes(y = temp, col = "temp")) +
+  scale_x_datetime(date_breaks = "1 month",
+                   date_labels = "%b %Y") +
+  scale_color_manual(labels = c("Dissolved oxygen",
+                                "Water temperature"),
+                     values = c("darkorange","royalblue")) + 
+  labs(x = "",
+       y = bquote("Dissolve oxygen (mg l"^-1*") and Water temperature (°C)"),
+       col = "") + 
+  tema -> oxy_temp_plot
+
+tiff("Figures/Figure S2.tiff", height = 400, width = 1400)
+oxy_temp_plot 
+dev.off()
